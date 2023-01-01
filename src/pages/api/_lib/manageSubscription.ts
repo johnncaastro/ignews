ddd60ce87c16a2e24faddd207d46c57fe6a948b1
Jonathan Castro Alves <jonathan.calves74@gmail.com>
2023-01-01 20:49:06 -0300

    Update manageSubscription.ts

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,7 +5,7 @@ import { stripe } from "../../../services/stripe";
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string,
-  createAction: false
+  createAction: boolean
 ) {
   const userRef = await fauna.query(
     q.Select(
